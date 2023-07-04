6a34c590d7dbbc8fc7ecff70773cc0fde6b8f47c
ykkssyaa <mr.ender.03@mail.ru>
2023-07-04 22:05:29 +0300

    main_menu, processing messages in email + update emailHandler.py

--- emailHandler.py
+++ emailHandler.py
@@ -1,6 +1,6 @@
 import imaplib
 import email
-
+from email.header import decode_header
 
 class MailBox:
     SMTP_SERVER = "imap.mail.ru"
@@ -8,18 +8,20 @@ class MailBox:
     USER = "mr.ender.03@mail.ru"
     PASSWORD = "m78pQdtJQ9vn6UAJPEUa"
 
-    def __init__(self):
+    def __init__(self, flag: str):
         self.imap = imaplib.IMAP4_SSL(host=self.SMTP_SERVER, port=self.SMTP_PORT)
         self.imap.login(self.USER, self.PASSWORD)
+        self.flag = flag
 
     def __enter__(self):
-        self.emails = self._get_all_messages()
+        self.emails = self._get_messages_with_flag(self.flag)
+        return self
 
-    def __exit__(self):
+    def __exit__(self,  exp_type, exp_value, exp_tr):
         self.imap.close()
         self.imap.logout()
 
-    def fetch_message(self, num=-1):
+    def fetch_message(self, num=0):
         status, data = self.imap.fetch(self.emails[num], '(RFC822)')
 
         if status != 'OK':
@@ -29,37 +31,32 @@ class MailBox:
         email_message = email.message_from_bytes(bytes_data)
         return email_message
 
-    def get_attachment(self, num=-1):
+    def get_attachment(self, num=0):
+        res = []
         for part in self.fetch_message(num).walk():
             if part.get_content_maintype() == 'multipart' or part.get('Content-Disposition') is None:
                 continue
             if part.get_filename():
-                return [part.get_filename(), part.get_payload(decode=True).strip()]
 
-    def _get_all_messages(self, count=-1):
+                try:
+                    res.append([decode_header(part.get_filename())[0][0].decode(),
+                                part.get_payload(decode=True).strip()])
+                except:
+                    res.append([part.get_filename(), part.get_payload(decode=True).strip()])
+
+        return res
+
+    def _get_messages_with_flag(self, flag="ALL", count=-1):
         self.imap.select('Inbox')
-        status, data = self.imap.search(None, 'ALL')
+        status, data = self.imap.search(None, flag)
 
         if status != 'OK':
             raise NameError("Error with parsing email")
 
         res = data[0].split()
 
-        if len(res) < count:
+        if len(res) < count or count == -1:
             return res
 
         return res[:count]
 
-    def _get_unseen_messages(self, count=-1):
-        self.imap.select('Inbox')
-        status, data = self.imap.search(None, 'UNSEEN')
-
-        if status != 'OK':
-            raise NameError("Error with parsing email")
-
-        res = data[0].split()
-
-        if len(res) < count:
-            return res
-
-        return res[:count]
--- main.py
+++ main.py
@@ -1,71 +1,158 @@
+#! /usr/bin/env python
+# -*- coding: windows-1251 -*-
 import email
 import imaplib
 from email.header import decode_header
+from datetime import datetime, timedelta
 
 import emailHandler
 
-#  РЎРѕС…СЂР°РЅРµРЅРёРµ Р·Р°РІРёСЃРёРјРѕСЃС‚РµР№ -  pip freeze > requirements.txt
+#  Сохранение зависимостей -  pip freeze > requirements.txt
 
-# TODO: СЃРґРµР»Р°С‚СЊ С‡РµСЂРµР· РЅР°СЃС‚СЂР°РёРІР°РµРјС‹Р№ РєРѕСЂС„РёРі
+# TODO: сделать через настраиваемый корфиг
 mail_pass = "m78pQdtJQ9vn6UAJPEUa"
 username = "mr.ender.03@mail.ru"
 imap_server = "imap.mail.ru"
 imap_port = 993
+doc_dir = "F:\\IT\\AntiPlagiat"
+valid_file_extensions = ["docx", "pdf"]
 
-# РџРѕРґРєР»СЋС‡РµРЅРёРµ Рє РїРѕС‡С‚Рµ
+DAYS_COUNT = 7
+
+# TODO: убрать
+# Подключение к почте
 imap = imaplib.IMAP4_SSL(host=imap_server, port=imap_port)
 imap.login(username, mail_pass)
 
-filename = "file.docx"
 
-def main():
+def m_main():
     select_resp = imap.select("INBOX")
     mails_count = int(select_resp[1][0])
 
-    # TODO: РџСЂРѕРІРµСЂРєР° РЅР° РєРѕСЂСЂРµРєС‚РЅРѕСЃС‚СЊ РїРѕРґРєР»СЋС‡РµРЅРёСЏ
+    # TODO: Проверка на корректность подключения
     print(f"Start - Status:<{select_resp[0]}>, mails count:{mails_count}")
 
     res, msg = imap.fetch(b'858', '(RFC822)')
     msg = email.message_from_bytes(msg[0][1])
 
-    # РґР°С‚Р° РїРѕР»СѓС‡РµРЅРёСЏ, РїСЂРёС…РѕРґРёС‚ РІ РІРёРґРµ СЃС‚СЂРѕРєРё, РґР°Р»СЊС€Рµ РЅР°РґРѕ РµС‘ РїР°СЂСЃРёС‚СЊ РІ С„РѕСЂРјР°С‚ datetime
+    # дата получения, приходит в виде строки, дальше надо её парсить в формат datetime
     letter_date = email.utils.parsedate_tz(msg["Date"])
-    letter_id = msg["Message-ID"]  # Р°Р№РґРё РїРёСЃСЊРјР°
+    letter_id = msg["Message-ID"]  # айди письма
     letter_from = msg["Return-path"]
 
     print(f"Date: {letter_date}, ID: {letter_id}, FROM: {letter_from}\n")
 
-    # TODO: РџСЂРѕРІРµСЂРєР° РЅР° РїСѓСЃС‚РѕРµ РЅР°Р·РІР°РЅРёРµ
-    header = decode_header(msg["Subject"])[0][0].decode()  # Р”РµРєРѕРґРёСЂРѕРІР°РЅРёРµ РЅР°Р·РІР°РЅРёСЏ РїРёСЃСЊРјР°
+    # TODO: Проверка на пустое название
+    header = decode_header(msg["Subject"])[0][0].decode()  # Декодирование названия письма
     print(header)
 
     for part in msg.walk():
 
         if part.get_content_disposition() == 'attachment':
-            print(decode_header(part.get_filename())[0][0].decode())  # РџРѕР»СѓС‡РµРЅРёРµ РЅР°Р·РІР°РЅРёСЏ С„Р°Р№Р»Р°
+            print(decode_header(part.get_filename())[0][0].decode())  # Получение названия файла
 
-            with open(filename, "wb") as f:
-                f.write(part.get_payload(decode=True))  # РЎРѕС…СЂР°РЅРµРЅРёРµ С„Р°Р№Р»Р°
+            with open(decode_header(part.get_filename())[0][0].decode(), "wb") as f:
+                f.write(part.get_payload(decode=True))  # Сохранение файла
+
+
+def main_menu():
+    print("Меню:")
+    print("1. Просмотр писем за последнюю неделю")  # TODO: реализовать
+    print("2. Просмотр непрочитанных писем за последнюю неделю")  # TODO: реализовать
+    print("3. Просмотр непрочитанных писем за последние N дней")  # TODO: реализовать
+    print("4. Данные в конфиге")
+    print("0. Выход")
+
+    return input("Ваш выбор: ")
+
+
+def config_info():
+    pass  # TODO: реализовать
+
+
+def isFileValid(filename: str):
+    for extension in valid_file_extensions:
+        if extension in filename:
+            return True
+    return False
+
+
+# Удаление файлов из списка с невалидным расширением
+def deleteNotValidFiles(attachment):
+    for i in range(len(attachment)):
+        if not isFileValid(attachment[i][0]):
+            attachment.pop(i)
+
+    return attachment
+
+
+# Получение темы письма и даты получения
+def message_processing(msg):
+    date = email.utils.parsedate_tz(msg["Date"])
+
+    present = datetime.now()
+    date = datetime(date[0], date[1], date[2])
+
+    if (present - date).days > DAYS_COUNT:
+        raise NameError("End processing")
+
+    title = ""
+
+    try:
+        title = (decode_header(msg["Subject"])[0][0].decode())
+    except AttributeError:
+        title = (decode_header(msg["Subject"])[0][0])
+    except TypeError:
+        title = "Без Темы"
+    except UnicodeDecodeError:
+        title = decode_header(msg["Subject"])[0][0].decode("windows-1251")
+    except:
+        title = "Ошибка получения темы"
+
+    return [date.date(), title]
+
+
+# Проход по всем письмам, которые были получены в течение недели
+def check_mail_last_week(flag: str):
+    try:
+        with emailHandler.MailBox(flag) as mail:
+            for msg_num in range(1, len(mail.emails) + 1):
+                try:
+                    # Считываю с письма вложения и удаляю с ненужным расширением
+                    attachment = deleteNotValidFiles(mail.get_attachment(-msg_num))
+                    if len(attachment) == 0:  # Если вложений нет, пропускаю письмо
+                        continue
+
+                    # Считываю тему письма и дату получения
+                    date, title = message_processing(mail.fetch_message(-msg_num))
+                    print(title, attachment[0][0])
+
+                except NameError:
+                    break
+
+    except NameError as err:
+        print(err.name)
+
+
+# Главная функция
+def main():
+    # TODO: Hello messages - config data
+    print("")
+
+    while True:
+        user_input = main_menu()
+
+        match user_input:
+            case '1':
+                check_mail_last_week("ALL")
+            case '2':
+                check_mail_last_week("UNSEEN")
+            case '3':
+                pass
+            case '0':
+                exit(0)
 
 
 if __name__ == '__main__':
-    # main()
-    mail = emailHandler.MailBox()
-    mail.__enter__()
-
-    msg = mail.fetch_message()
-    title = decode_header(msg["Subject"])[0][0].decode()
-    print(title)
-
-    with open(filename, "wb") as f:
-        f.write(mail.get_attachment()[1])
-
-    mail.__exit__()
-
-
-
-
-
-
-
-
+    main()
+    exit(0)
